eb11b4c74ea0ec0223ba1b843daf51f0d0f195af
Brian Munsey <brianmunsey2018@gmail.com>
2022-04-17 15:08:12 -0400

    added photos, changed css, started some functionality

---
+++ js/app.js
@@ -0,0 +1,8 @@
+const mainContent = document.querySelector(".mainContent");
+const contactPage = document.querySelector("#contactUs");
+const projectsPage = document.querySelector("#projects");
+const servicesPage = document.querySelector("#services");
+
+contactPage.addEventListener("click", () => {
+    alert("hello");
+});
